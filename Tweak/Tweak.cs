b1c2216bd2fa693468d1b7b149bfd32af7a5e732
VladimirR <rvitaminka20@gmail.com>
2026-07-06 15:10:32 +0300

    Update Tweak.cs

--- Tweak/Tweak.cs
+++ Tweak/Tweak.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+﻿using HarmonyLib;
 using OutPathOptionsMod.Configuration;
 using OutPathOptionsMod.Configuration.ConfigurationElements;
 using System.Collections.Generic;
@@ -8,12 +8,15 @@ namespace OutPathOptionsMod.Tweaks
 {
     public class Tweak : MonoBehaviour, IConfigureObject
     {
+        protected Harmony _harmony;
+
         protected List<ConfigurationElement> _configurations;
 
         public List<ConfigurationElement> GetConfigurations() => _configurations;
 
         public virtual void Init(OutPathOptionsMod plugin)
         {
+            _harmony = plugin.GetHarmony();
             _configurations = new List<ConfigurationElement>();
         }
     }
