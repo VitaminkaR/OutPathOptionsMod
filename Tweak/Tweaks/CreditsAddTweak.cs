f7602d32a63eaee5c3662bf4479f9c5ee77beb64
VladimirR <rvitaminka20@gmail.com>
2026-07-07 22:50:50 +0300

    Update CreditsAddTweak.cs

--- Tweak/Tweaks/CreditsAddTweak.cs
+++ Tweak/Tweaks/CreditsAddTweak.cs
@@ -16,7 +16,7 @@ namespace OutPathOptionsMod.Tweaks
             base.Init(plugin);
 
             HeaderConfigurationElement.Create(GetConfigurations(), $"{Name}_header", "[ADD CREDITS]");
-            _key = KeyConfigurationElement.Create(GetConfigurations(), Name + "_toggle", "Add", UnityEngine.KeyCode.P);
+            _key = KeyConfigurationElement.Create(GetConfigurations(), Name + "_toggle", "Add Key", KeyCode.P);
             _value = IntConfigurationElement.Create(GetConfigurations(), Name + "_credits", "Count", 100, 0, int.MaxValue);
 
             _harmony.PatchAll(typeof(CreditsAddPatches));
