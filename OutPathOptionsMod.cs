72e98128bef689e51bf1291726f3d6bd16b0b6b1
VladimirR <rvitaminka20@gmail.com>
2026-07-08 13:39:07 +0300

    Infinite Battery Tweak

--- OutPathOptionsMod.cs
+++ OutPathOptionsMod.cs
@@ -23,7 +23,6 @@ namespace OutPathOptionsMod
         public PluginInfo PluginInfo { get; } = new PluginInfo();
 
         // Build
-        public static ConfigEntry<bool> configInfiniteBattery;
         public static ConfigEntry<bool> configInstantBreak;
         public static ConfigEntry<float> configRadiusMult;
         public static ConfigEntry<float> configSpeedMult;
@@ -76,7 +75,6 @@ namespace OutPathOptionsMod
             }
 
             // Build
-            configInfiniteBattery = ((BaseUnityPlugin)this).Config.Bind<bool>("Builds", "Infinity Battery", false, "Sets whether buildings will work without a charge.");
             configInstantBreak = ((BaseUnityPlugin)this).Config.Bind<bool>("Builds", "Instant Break", false, "Sets whether mining buildings will instantly break down objects.");
             configRadiusMult = ((BaseUnityPlugin)this).Config.Bind<float>("Builds", "Radius Mutiplier", 0, "Sets the radius of buildings (0 - off) !Setting up requires restarting the world!.");
             configSpeedMult = ((BaseUnityPlugin)this).Config.Bind<float>("Builds", "Speed Mutiplier", 0, "Sets an increase in the speed of buildings by n times (0 - off) !Setting up requires restarting the world!.");
@@ -154,117 +152,5 @@ namespace OutPathOptionsMod
                 }
             }
         }
-
-        #region INFINITY_ENERGY_PATCHES
-        [HarmonyPatch(typeof(Build_WearStation), "UseEnergy")]
-        private class HarmonyPatch_WearStation_UseEnergy
-        {
-            private static void Prefix(Build_WearStation __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_BreakerManual), "UseEnergy")]
-        private class HarmonyPatch_BreakerManual_UseEnergy
-        {
-            private static void Prefix(Build_BreakerManual __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_ChlorophyllConverter), "UseEnergy")]
-        private class HarmonyPatch_ChlorophyllConverter_UseEnergy
-        {
-            private static void Prefix(Build_ChlorophyllConverter __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_ChlorophyllExtractor), "UseEnergy")]
-        private class HarmonyPatch_ChlorophyllExtractor_UseEnergy
-        {
-            private static void Prefix(Build_ChlorophyllExtractor __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_OreExtractor), "Update_Extracting")]
-        private class HarmonyPatch_OreExtractor_Update_Extracting
-        {
-            private static void Prefix(Build_OreExtractor __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_SlayerManual), "UseEnergy")]
-        private class HarmonyPatch_Slayer_UseEnergy
-        {
-            private static void Prefix(Build_SlayerManual __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_SoilMiner), "UseEnergy")]
-        private class HarmonyPatch_SoilMiner_UseEnergy
-        {
-            private static void Prefix(Build_SoilMiner __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_SoilMinerV2), "UseEnergy")]
-        private class HarmonyPatch_SoilMinerV2_UseEnergy
-        {
-            private static void Prefix(Build_SoilMinerV2 __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_Trapper), "UseEnergy")]
-        private class HarmonyPatch_Trapper_UseEnergy
-        {
-            private static void Prefix(Build_Trapper __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_Vaporizer), "UseEnergy")]
-        private class HarmonyPatch_Vaporizer_UseEnergy
-        {
-            private static void Prefix(Build_Vaporizer __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-
-        [HarmonyPatch(typeof(Build_WaterPump_Manual), "UseEnergy")]
-        private class HarmonyPatch_WaterPump_UseEnergy
-        {
-            private static void Prefix(Build_WaterPump_Manual __instance)
-            {
-                if (configInfiniteBattery.Value)
-                    __instance.energy = __instance.maxEnergy;
-            }
-        }
-        #endregion
     }
 }
---
+++ Tweak/Tweaks/InfiniteEnergyTweak.cs
@@ -0,0 +1,139 @@
+﻿using HarmonyLib;
+using OutPathOptionsMod.Configuration.ConfigurationElements;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OutPathOptionsMod.Tweaks
+{
+    [Tweak(Name = "InfiniteEnergy", Category = "Builds", ID = 1)]
+    public class InfiniteEnergyTweak : Tweak
+    {
+        public override void Init(OutPathOptionsMod plugin)
+        {
+            base.Init(plugin);
+
+            HeaderConfigurationElement.Create(GetConfigurations(), $"{Name}_header", "[INFINITE ENERGY]");
+            var toggle = BoolConfigurationElement.Create(GetConfigurations(), Name, "Toggle", false);
+            Activate(toggle.Value);
+            toggle.OnChangeValue += (bool v) => Activate(v);
+        }
+
+        private void Activate(bool v)
+        {
+            if (v)
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                var types = assembly.GetTypes().Where(t => t.Name.StartsWith("InfiniteEnergyPatches_"));
+                foreach (var type in types)
+                {
+                    _harmony.PatchAll(type);
+                }
+            }
+            else
+            {
+                _harmony.UnpatchSelf();
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_WearStation), "UseEnergy")]
+        private class InfiniteEnergyPatches_WearStation
+        {
+            private static void Prefix(Build_WearStation __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_BreakerManual), "UseEnergy")]
+        private class InfiniteEnergyPatches_BreakerManual
+        {
+            private static void Prefix(Build_BreakerManual __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_ChlorophyllConverter), "UseEnergy")]
+        private class InfiniteEnergyPatches_ChlorophyllConverter
+        {
+            private static void Prefix(Build_ChlorophyllConverter __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_ChlorophyllExtractor), "UseEnergy")]
+        private class InfiniteEnergyPatches_ChlorophyllExtractor
+        {
+            private static void Prefix(Build_ChlorophyllExtractor __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_OreExtractor), "Update_Extracting")]
+        private class InfiniteEnergyPatches_OreExtractor_Update_Extracting
+        {
+            private static void Prefix(Build_OreExtractor __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_SlayerManual), "UseEnergy")]
+        private class InfiniteEnergyPatches_Slayer
+        {
+            private static void Prefix(Build_SlayerManual __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_SoilMiner), "UseEnergy")]
+        private class InfiniteEnergyPatches_SoilMiner
+        {
+            private static void Prefix(Build_SoilMiner __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_SoilMinerV2), "UseEnergy")]
+        private class InfiniteEnergyPatches_SoilMinerV2
+        {
+            private static void Prefix(Build_SoilMinerV2 __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_Trapper), "UseEnergy")]
+        private class InfiniteEnergyPatches_Trapper
+        {
+            private static void Prefix(Build_Trapper __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_Vaporizer), "UseEnergy")]
+        private class InfiniteEnergyPatches_Vaporizer
+        {
+            private static void Prefix(Build_Vaporizer __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+
+        [HarmonyPatch(typeof(Build_WaterPump_Manual), "UseEnergy")]
+        private class InfiniteEnergyPatches_WaterPump
+        {
+            private static void Prefix(Build_WaterPump_Manual __instance)
+            {
+                __instance.energy = __instance.maxEnergy;
+            }
+        }
+    }
+}
