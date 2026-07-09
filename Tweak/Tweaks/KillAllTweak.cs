840e3fb7210b43cba72fe9c490787a0f1be440e6
VladimirR <rvitaminka20@gmail.com>
2026-07-09 17:59:13 +0300

    Kill All Tweak

---
+++ Tweak/Tweaks/KillAllTweak.cs
@@ -0,0 +1,36 @@
+﻿using OutPathOptionsMod.Configuration.ConfigurationElements;
+using UnityEngine;
+
+namespace OutPathOptionsMod.Tweaks
+{
+    [Tweak(Name = "KillAll", Category = "Resources", ID = 2)]
+    public class KillAllTweak : Tweak
+    {
+        private KeyConfigurationElement _key;
+
+        public override void Init(OutPathOptionsMod plugin)
+        {
+            base.Init(plugin);
+
+            HeaderConfigurationElement.Create(GetConfigurations(), $"{Name}_header", "[KILL ALL]");
+            _key = KeyConfigurationElement.Create(GetConfigurations(), Name + "_toggle", "Kill Key", KeyCode.None);
+            ButtonConfigurationElement.Create(GetConfigurations(), Name + "_button", "Kill").OnChangeValue += Kill;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_key.Value))
+            {
+                Kill();
+            }
+        }
+
+        private void Kill()
+        {
+            foreach (var item in FindObjectsOfType<EnemyHealth>())
+            {
+                item.DamageEnemy_General(float.MaxValue);
+            }
+        }
+    }
+}
