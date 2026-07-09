6fa305c478dd69f549e68098ab0795501a73f23e
VladimirR <rvitaminka20@gmail.com>
2026-07-09 17:49:29 +0300

    Destroy All Tweak

---
+++ Tweak/Tweaks/DestroyAllTweak.cs
@@ -0,0 +1,36 @@
+﻿using OutPathOptionsMod.Configuration.ConfigurationElements;
+using UnityEngine;
+
+namespace OutPathOptionsMod.Tweaks
+{
+    [Tweak(Name = "DestroyAll", Category = "Resources", ID = 1)]
+    public class DestroyAllTweak : Tweak
+    {
+        private KeyConfigurationElement _key;
+
+        public override void Init(OutPathOptionsMod plugin)
+        {
+            base.Init(plugin);
+
+            HeaderConfigurationElement.Create(GetConfigurations(), $"{Name}_header", "[DESTROY ALL]");
+            _key = KeyConfigurationElement.Create(GetConfigurations(), Name + "_toggle", "Add Key", KeyCode.None);
+            ButtonConfigurationElement.Create(GetConfigurations(), Name + "_button", "Destroy").OnChangeValue += Destroy;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_key.Value))
+            {
+                Destroy();
+            }
+        }
+
+        private void Destroy()
+        {
+            foreach (var item in FindObjectsOfType<TakeOutResource>())
+            {
+                item.TryTakeOut_General(float.MaxValue);
+            }
+        }
+    }
+}
