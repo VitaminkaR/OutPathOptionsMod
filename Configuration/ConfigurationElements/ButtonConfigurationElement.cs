8099076984de65296b9ab9ba10ca0b2ff436305b
VladimirR <rvitaminka20@gmail.com>
2026-07-09 17:49:21 +0300

    Button Configuration Element

---
+++ Configuration/ConfigurationElements/ButtonConfigurationElement.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OutPathOptionsMod.Configuration.ConfigurationElements
+{
+    public class ButtonConfigurationElement : ConfigurationElement
+    {
+        private string _title;
+
+        public event Action OnChangeValue;
+
+        private ButtonConfigurationElement(
+            List<ConfigurationElement> configurations,
+            string id,
+            string title) : base(id)
+        {
+            configurations.Add(this);
+            _title = title;
+        }
+
+        public static ButtonConfigurationElement Create(
+            List<ConfigurationElement> configurations,
+            string id,
+            string title)
+        {
+            ButtonConfigurationElement element = new ButtonConfigurationElement(configurations, id, title);
+            return element;
+        }
+
+        public override void Draw()
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(_title)) OnChangeValue.Invoke();
+            GUILayout.EndHorizontal();
+        }
+    }
+}
