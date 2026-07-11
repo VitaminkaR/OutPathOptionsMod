fc09f85f9b00ede99be059f5e4f9dedbe91a6123
VladimirR <rvitaminka20@gmail.com>
2026-07-11 21:02:51 +0300

    Infinite Effect

---
+++ Tweak/Tweaks/InfiniteEffectTweak.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using OutPathOptionsMod.Configuration.ConfigurationElements;
+
+namespace OutPathOptionsMod.Tweaks
+{
+    [Tweak(Name = "InfiniteEffect", Category = "Player")]
+    public class InfiniteEffectTweak : Tweak
+    {
+        private static BoolConfigurationElement _toggle;
+
+        public override void Init(OutPathOptionsMod plugin)
+        {
+            base.Init(plugin);
+
+            HeaderConfigurationElement.Create(GetConfigurations(), $"{Name}_header", "[INFINITE EFFECTS]");
+            _toggle = BoolConfigurationElement.Create(GetConfigurations(), Name, "Toggle", false);
+        }
+
+        [HarmonyPatch(typeof(StatusTimer), "Update")]
+        private static class InfiniteEffectPatches
+        {
+            private static void Prefix(StatusTimer __instance)
+            {
+                if (_toggle.Value)
+                    __instance.timeToDestroy = __instance.itemInfo.duration;
+            }
+        }
+    }
+}
